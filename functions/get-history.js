030d4bd419de00cb320d99c07a6d3941277a2369
omidmohammad2020-svg <omid.mohammad2020@gmail.com>
2026-08-06 23:22:08 +0400

    Add files via upload

---
+++ functions/get-history.js
@@ -0,0 +1,25 @@
+const { getStore } = require('@netlify/blobs');
+
+function getSignalStore() {
+  return getStore({
+    name: 'signal-tracker',
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_BLOBS_TOKEN
+  });
+}
+
+exports.handler = async function () {
+  const store = getSignalStore();
+  const history = (await store.get('history', { type: 'json' })) || [];
+  const weekly = (await store.get('weekly', { type: 'json' })) || { signals: 0, tp: 0, sl: 0, invalidated: 0, weekStart: Date.now() };
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-store'
+    },
+    body: JSON.stringify({ history, weekly })
+  };
+};
